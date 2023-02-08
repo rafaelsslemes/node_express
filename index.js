c23aba9240ce5980353d46abf62bd4fedef91c74
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-08 16:24:31 -0300

    Using get method Params

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const port = 8080;
 
 const basePath = path.join(__dirname, 'templates');
 
+// Use an middleware to simulate auth. Similar to design pattern 'decorator'
 app.use(authentication)
+
+// Base route configuration
 app.get('/', (request, response) => {
-
     response.sendFile(basePath + '/index.html');
+});
 
+// User route configuration
+app.get('/user/:id', (request, response) => {
+    const id = request.params.id;
+    console.log(`Select user ID:${id}.`);
+    response.sendFile(basePath + '/user.html');
 });
 
 app.listen(port, ()=>{
