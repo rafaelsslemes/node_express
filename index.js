39fc281892f098e400f2f6b8525d30e7dc9994dc
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-08 16:39:17 -0300

    Receiving request body by Post method

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const port = 8080;
 const basePath = path.join(__dirname, 'templates');
 
 // Use an middleware to simulate auth. Similar to design pattern 'decorator'
-app.use(authentication)
+app.use(authentication);
+
+// Middleware to read request body
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 // Base route configuration
 app.get('/', (request, response) => {
@@ -15,12 +19,23 @@ app.get('/', (request, response) => {
 });
 
 // User route configuration
+
+app.get('/user/register', (request, response) => {
+    response.sendFile(basePath + '/user_register.html');
+});
+
+app.post('/user/register', (request, response) => {
+    console.log(request.body);
+    response.sendFile(basePath + '/user.html');
+});
+
 app.get('/user/:id', (request, response) => {
     const id = request.params.id;
     console.log(`Select user ID:${id}.`);
     response.sendFile(basePath + '/user.html');
 });
 
+// Start server listening
 app.listen(port, ()=>{
     console.log(`Express Server Runing on port ${port}`);
 });
